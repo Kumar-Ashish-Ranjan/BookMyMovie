c0a576894fc4026e8f1acd1dd583327a67b7eede
Kumar-Ashish-Ranjan <83904643+Kumar-Ashish-Ranjan@users.noreply.github.com>
2023-09-24 12:10:46 +0530

    initial commit

---
+++ TheaterManagementService/src/test/java/com/bookmymovie/TheaterManagementService/TheaterManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmymovie.TheaterManagementService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheaterManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
